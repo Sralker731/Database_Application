98b96efd647ebe9783e73b5b289507194ad28a14
unknown <kennyman143@gmail.com>
2023-04-12 22:56:44 +0300

    adds UI part

--- database.py
+++ database.py
@@ -20,22 +20,3 @@ class Database:
         cur = connect.cursor()
         query_result = cur.execute(f'SELECT {column_list} from {object_name}')
         return query_result
-
-db = Database('random_shit')
-db.create_database()
-#db.create_query("""CREATE TABLE test(i integer)""")
-#db.create_query("""INSERT INTO test(i) VAlUES (1)""")
-
-print(db.select_object('test', '*'))
-
-# TODO
-1. Залить первую НЕДОверсию в гитхаб
-2. Поэкспериментировать с запросами в базу данных
-3. Решить баг, который создает базу вне рамок репозитория
-4. Создать функции (выбрать 1 объект, много и все)
-5. Создать файл exceptions.py и прописать ошибки:
-    Query Error 
-    Database\Table\Index not found error 
-6. Составить документацию по продукту
-7. Залить отдельной веткой (db)
-
