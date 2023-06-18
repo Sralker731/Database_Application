0a3da0ea3cf3dbfab4b958551eab6d5646e23197
Rudenko_Kostya2010 <rudenkokostya10@gmail.com>
2023-06-18 12:12:42 +0300

    Tested select func.

--- engine.py
+++ engine.py
@@ -34,8 +34,8 @@ def save_txt_file(data, file_name):
         file.close()
 
 def find_select_stmt(query):
-    select_values = re.findall(SELECT_QUERY, query)
+    select_values = re.match(SELECT_QUERY, query).group()
     select_query = ''
-    for value in select_values:
-        select_query += value + '\n'
-    return select_query
+    #for value in select_values:
+    #    select_query += value + '\n'
+    return select_values
